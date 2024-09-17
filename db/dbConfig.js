39e264e6a92e50d3664a7d0375bc9852e236c994
SharukhGV <sharukh.g.velupillai@gmail.com>
2024-09-17 18:24:50 -0400

    added connection string alternative for prod. build vs local to use neon db

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,19 +1,26 @@
-
 const pgp = require("pg-promise")();
 require("dotenv").config();
 
-const cn = {
+let db;
+
+if (process.env.DATABASE_URL) {
+  //FOR production on Render
+  db = pgp(process.env.DATABASE_URL);
+} else {
+  // For local development
+  const cn = {
     host: process.env.PG_HOST,
     port: process.env.PG_PORT,
     database: process.env.PG_DATABASE,
     user: process.env.PG_USER,
     password: process.env.PG_PW,
-    max: 30 
+    max: 30,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  };
+  
+  db = pgp(cn);
+}
 
-};
-console.log(cn)
-
-
-const db = pgp(cn);
+console.log("Database connection established");
 
 module.exports = db;
